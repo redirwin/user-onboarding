db0c5165b6d14a5840bf7923b2fd187ac5f40c04
Dave Irwin <david@redirwin.com>
2019-07-17 12:04:41 -0700

    Installed smooth-ui for stlyled-components modules. Added Normalize.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+import { Normalize } from "@smooth-ui/core-sc";
 
 import Form from "./components/Form";
 
 export default function App() {
   return (
     <div className="App">
+      <Normalize />
       <Form />
     </div>
   );
